59a9b8fd6d76551df225678e5cd1dd084c1a4d8b
osher17 <72805121+osher17@users.noreply.github.com>
2020-10-13 19:07:02 +0300

    Add files via upload
    זה הקובץ המעודכן

--- MainActivity.java
+++ MainActivity.java
@@ -1,5 +1,6 @@
-package com.example.locationtryfirst;
+package com.example.findlocationbasic;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 
@@ -13,120 +14,120 @@ import android.view.View;
 import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.widget.ToggleButton;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.tasks.OnSuccessListener;
 
+import static android.widget.Toast.LENGTH_SHORT;
+
 public class MainActivity extends AppCompatActivity {
 
-    private static final int DEFAULT_INTERVAL = 30000; // default time for an update
-    private static final int FASTEST_INTERVAL = 5000; // fastest time for an update
-    private static final int PERMISSION_FINE_LOCATION = 22;
+    public static final long DEFAULT_UPDATE_INTERVAL = 30000;
+    public static final long FASTEST_UPDATE_INTERVAL = 5000;
+    public static final int PERMISSION_FINE_LOCATION = 22;
 
-    TextView lat_res, long_res, type_txt; // text views which will show the location and the settings (type of location collecting)
-    Switch type_sw, prm_sw; // settings switches
-    LocationRequest locationRequest; // saves the configurations of the request
-    FusedLocationProviderClient fusedLocationClient; // Google's API for location services
-    boolean batterySaver; // is on battery saver mode? (doesn't use gps but cellphone tower tracing)
-    double latitute, longitude;
-    String toastStr;
-
+    Switch type_sw; // switch responsible for settings
+    TextView type_txt; // presents which type of location tracking is used
+    TextView address_txt ; // presents address
+    ToggleButton prm_button; // stop and start the tracking
+    FusedLocationProviderClient fusedLocationProviderClient;
+    LocationRequest locationRequest;
+    Double longitude, latitude;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //give a value to each UI variable
-        lat_res = (TextView) findViewById(R.id.lat_res);
-        long_res = (TextView) findViewById(R.id.long_res);
+        type_sw = (Switch)findViewById(R.id.type_sw);
         type_txt = (TextView)findViewById(R.id.type_txt);
-        type_sw = (Switch) findViewById(R.id.type_sw);
-        prm_sw = (Switch) findViewById(R.id.prm_sw);
+        address_txt = (TextView)findViewById(R.id.address_txt);
+        prm_button = (ToggleButton)findViewById(R.id.prm_btn);
 
-
-        // create an instance of location request and set it to the wanted settings
         locationRequest = new LocationRequest();
-        locationRequest.setInterval(DEFAULT_INTERVAL);
-        locationRequest.setFastestInterval(FASTEST_INTERVAL);
-        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        locationRequest.setInterval(DEFAULT_UPDATE_INTERVAL);
+        locationRequest.setFastestInterval(FASTEST_UPDATE_INTERVAL);
+        locationRequest.setPriority(LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY);
 
-        // set and manage switch listener
         type_sw.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                if (type_sw.isChecked()) {
+            public void onClick(View v) {
+                if(type_sw.isChecked())
+                {
                     locationRequest.setPriority(LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY);
-                    type_txt.setText("Using cell towers");
-                } else {
+                    type_txt.setText("Using towers + wifi");
+                }
+                else
+                {
                     locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
-                    type_txt.setText("Using GPS");
+                    type_txt.setText("Using GPS sensors");
+
                 }
             }
         });
 
-        //
-        //fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
-        Log.i("Main Activity", "On create");
         dealWithLocation();
+
 
     }
 
+
+
     @Override
-    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults)
-    {
-        super.onRequestPermissionsResult(requestCode,permissions,grantResults);
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
         switch(requestCode)
         {
             case PERMISSION_FINE_LOCATION:
-                if(grantResults[0]==PackageManager.PERMISSION_GRANTED)
+                if(grantResults[0] == PackageManager.PERMISSION_GRANTED)
                 {
-                    Log.i("MainActivity", "Got Location Permission");
                     dealWithLocation();
                 }
                 else
                 {
-                    Toast.makeText(this, "didn't get fine location permission", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(this,"this app requires location permission", LENGTH_SHORT).show();
                 }
                 break;
         }
     }
 
-    private void dealWithLocation()
+    public void dealWithLocation()
     {
-        fusedLocationClient = LocationServices.getFusedLocationProviderClient(MainActivity.this);
-        // check if we have permission
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // if we don't
-            // check if build version sufficient 23 or over
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M)
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(MainActivity.this);
+        if(ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED)
+        {
+            fusedLocationProviderClient.getLastLocation().addOnSuccessListener(this, new OnSuccessListener<Location>() {
+                @Override
+                public void onSuccess(Location location)
+                {
+                    if(location != null) {
+                        // we got permissions
+                        updateUi(location);
+                    }
+                    else
+                    {
+                        Log.i("We have permission", "location is null***************************************************");
+                    }
+                }
+            });
+        }
+        else
+        {
+            if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M)
             {
-                // request permission
-                requestPermissions( new String[] {Manifest.permission.ACCESS_FINE_LOCATION}, PERMISSION_FINE_LOCATION);
+                requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, PERMISSION_FINE_LOCATION);
             }
-            Log.i("Main Activity", "Asking for permission");
-            return;
         }
-        else {
-            Log.i("MainActivity", "Trying to receive loaction");
-            fusedLocationClient.getLastLocation().addOnSuccessListener(this, new OnSuccessListener<Location>() {
-                        @Override
-                        public void onSuccess(Location location) {
-                            if (location != null) {
-                                Log.i("Main Activity", "Got location, not null");
-                                latitute = location.getLatitude();
-                                longitude = location.getLongitude();
-                                toastStr = "latitute: " + latitute + " longitude: " + longitude;
-                                Toast.makeText(MainActivity.this, toastStr, Toast.LENGTH_SHORT).show();
-                            }
-                            else {
-                                Log.i("Main Activity", "Location is null");
+    }
 
-                                Toast.makeText(MainActivity.this, "something went wrong :/ ", Toast.LENGTH_SHORT).show();
-                            }
-                        }
-                    });
-        }
+    private void updateUi(Location location)
+    {
+        longitude = location.getLongitude();
+        latitude = location.getLatitude();
+        Toast.makeText(this, "Latitude: " + latitude + " Longitude: " + longitude, LENGTH_SHORT).show();
+
     }
 }
